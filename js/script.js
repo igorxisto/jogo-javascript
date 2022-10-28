710cc4ffdff5d223f4cf5f2fd3f1a94544ed9544
Igor Xisto <webdeveloperigorxisto@gmail.com>
2022-10-28 18:12:07 -0300

    v1
     joguinho em java script

---
+++ js/script.js
@@ -0,0 +1,36 @@
+ const mario = document.querySelector('.mario') //pegar a classe mario
+ const pipe = document.querySelector('.pipe') //pegar a classe pipe
+
+const jump= () =>{
+    mario.classList.add('jump'); //criar o pulo adicionando a classe jump na class mario
+    setTimeout(()=> {
+        mario.classList.remove('jump'); 
+    }, 500);
+}
+
+const loop = setInterval(()=>{
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+ 
+        pipe.style.animation = 'none'; 
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none'; 
+        mario.style.bottom  = `${marioPosition}px`;
+
+        // mario.src = './imagens/game-over.png';
+        mario.src = './imagens/pika.gif';
+        mario.style.width = "100px";
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump)
+
